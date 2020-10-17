497ae87990262888fa1a36d9831923c93f19179b
Chalita Ndunu <chalitandunu130@gmail.com>
2020-10-17 08:35:00 +0200

    Set up server and libraries

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const lodash = require("lodash");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    const name = "root";
+    res.render("index", {
+        name
+    });
+});
+
+app.listen(port, () => {
+    console.log(`GiveTheChange is running on port ${port}`);
+});
