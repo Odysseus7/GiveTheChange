7c5801ed00c41e594ddd0c5c416855915bb4901c
Chalita Ndunu <chalitandunu130@gmail.com>
2020-10-22 08:12:21 +0200

    set up database and admin page

--- app.js
+++ app.js
@@ -11,11 +11,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('src'))
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/changeDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    balance: Number
+});
+
+const User = new mongoose.model("User", userSchema);
+
 app.get('/', (req,res) => {
-    const name = "root";
-    res.render("index", {
-        name
+    // find admin user and show balance
+    User.findOne({username: "Chalita"}, (err, user) => {
+        if(user) {
+            username = user.username
+            balance = user.balance;
+            res.render("index", {
+                username,
+                balance
+            });
+        } else {
+            console.log("Admin not found");
+        }
     });
+});
+
+app.post('/', (req, res) => {
+    console.log((req.body.money).split("."));
 });
 
 app.listen(port, () => {
