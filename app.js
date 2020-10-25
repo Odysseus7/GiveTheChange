9d024829c955abc76443c476927f59cf378f2656
Odysseus7 <chalitandunu130@gmail.com>
2020-10-25 18:13:40 +0100

    view for login page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
-const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const math = require("mathjs");
 
 const app = express();
@@ -22,55 +23,9 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 app.get('/', (req,res) => {
-    // find admin user and show balance
-    User.findOne({username: "Chalita"}, (err, user) => {
-        if(user) {
-            username = user.username
-            balance = math.round(user.balance, 2);
-            res.render("index", {
-                username,
-                balance
-                
-            });
-        } else {
-            console.log("Admin not found");
-        }
-    });
+    res.render("login");
 });
 
-app.post('/', async (req, res) => {
-    // check if not meant for another person
-    if(!req.params.name) {
-        const change = calculateChange(req.body.money);
-        await User.findOneAndUpdate({username: "Chalita"}, {balance: balance + change});
-        res.redirect('/');
-    } else {
-        const name = _.capitalize(req.params.name);
-        res.redirect(`/${name}`);
-    }
-
-
-});
-
-app.get("/:name", (req, res) => {
-    username = _.capitalize(req.params.name);
-    User.findOne({ username }, (err, foundUser) => {
-        if(foundUser) {
-            res.render("index", {
-                username: foundUser.username,
-                balance: foundUser.balance
-            });
-        } else {
-            const user = new User({
-                username,
-                balance: 0
-            });
-
-            user.save();
-            res.redirect(`/${username}`);
-        }
-    });
-});
 
 function calculateChange(amount) {
     // Only get decimal part of change
